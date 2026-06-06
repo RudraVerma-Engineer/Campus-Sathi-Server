d070dfd41b19f5a49e24eb000f12b9ddb0afa577
Rudra Verma <rudrav580@gmail.com>
2026-06-06 18:51:10 +0530

    sender changes

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,20 +1,32 @@
-import nodemailer from "nodemailer";
+// import nodemailer from "nodemailer";
 
-export const sendEmail = async(email, subject, message)=>{
-    const transporter = nodemailer.createTransport({
-      host:"smtp.gmail.com",
-      port: 587,
-      secure: false, // STARTTLS (important)
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
+// export const sendEmail = async(email, subject, message)=>{
+//     const transporter = nodemailer.createTransport({
+//         service:"gmail",
 
-    await transporter.sendMail({
-        from : process.env.EMAIL_USER,
-        to: email,
-        subject,
-        text: message,
-    });
-}
+//         auth:{
+//             user:process.env.EMAIL_USER,
+//             pass: process.env.EMAIL_PASS,
+//         },
+//     });
+
+//     await transporter.sendMail({
+//         from : process.env.EMAIL_USER,
+//         to: email,
+//         subject,
+//         text: message,
+//     });
+// }
+
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export const sendEmail = async (email, subject, message) => {
+  await resend.emails.send({
+    from: "Campus Sathi <onboarding@resend.dev>",
+    to: email,
+    subject,
+    text: message,
+  });
+};
