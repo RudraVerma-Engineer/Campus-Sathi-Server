528efa50356ce409d4b96b6fdceb13ea17f836b3
Rudra Verma <rudrav580@gmail.com>
2026-06-06 19:12:19 +0530

    add nodemailer email services

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,32 +1,19 @@
-// import nodemailer from "nodemailer";
+import nodemailer from "nodemailer";
 
-// export const sendEmail = async(email, subject, message)=>{
-//     const transporter = nodemailer.createTransport({
-//         service:"gmail",
+export const sendEmail = async(email, subject, message)=>{
+    const transporter = nodemailer.createTransport({
+        service:"gmail",
 
-//         auth:{
-//             user:process.env.EMAIL_USER,
-//             pass: process.env.EMAIL_PASS,
-//         },
-//     });
+        auth:{
+            user:process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS,
+        },
+    });
 
-//     await transporter.sendMail({
-//         from : process.env.EMAIL_USER,
-//         to: email,
-//         subject,
-//         text: message,
-//     });
-// }
-
-import { Resend } from "resend";
-
-const resend = new Resend(process.env.RESEND_API_KEY);
-
-export const sendEmail = async (email, subject, message) => {
-  await resend.emails.send({
-    from: "Campus Sathi <onboarding@resend.dev>",
-    to: email,
-    subject,
-    text: message,
-  });
-};
+    await transporter.sendMail({
+        from : process.env.EMAIL_USER,
+        to: email,
+        subject,
+        text: message,
+    });
+}
