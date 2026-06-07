7eed747e07de6fd69b7ab55c6a73cba552ef7f85
Rudra Verma <rudrav580@gmail.com>
2026-06-08 05:03:13 +0530

    resolve

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -2,21 +2,12 @@ import nodemailer from "nodemailer";
 
 export const sendEmail = async(email, subject, message)=>{
     const transporter = nodemailer.createTransport({
-      // service:"gmail",
-      host: "smtp.gmail.com", // or your provider's host
-      port: 465, // use 587 (not 25, not 465 for most)
-      secure: true, // false for 587, true for 465
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    //   connectionTimeout: 10000, // 10 seconds
-    //   greetingTimeout: 10000,
-    //   socketTimeout: 10000,
-    //   auth: {
-    //     user: process.env.EMAIL_USER,
-    //     pass: process.env.EMAIL_PASS,
-    //   },
+        service:"gmail",
+
+        auth:{
+            user:process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS,
+        },
     });
 
     await transporter.sendMail({
